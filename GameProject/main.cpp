25c1964424d066d67972a2150832887a66660c28
mashi89 <aleshnar@gmail.com>
2026-03-07 17:34:09 +0200

    Normalize line endings to CRLF

---
+++ GameProject/main.cpp
@@ -0,0 +1,20 @@
+#include "include/core/Application.h"
+#include "include/utils/Logger.h"
+
+int main()
+{
+    Logger::GetInstance().Init("game.log");
+    Logger::GetInstance().Info("GameSolution starting...");
+
+    Application app;
+    if (!app.Initialize())
+    {
+        Logger::GetInstance().Error("Failed to initialize application.");
+        return -1;
+    }
+
+    app.Run();
+
+    Logger::GetInstance().Info("GameSolution exiting cleanly.");
+    return 0;
+}
